6483d23314180421e230190b3e778bf6fa60d7c3
saashqdev <saashqdev@gmail.com>
2025-06-17 15:17:53 -0400

    update homepage

--- src/app/(app)/(marketing)/page.tsx
+++ src/app/(app)/(marketing)/page.tsx
@@ -1,26 +1,22 @@
 import { AnimatedBeamSection } from '@/components/home/animated-beam'
 import { FeatureSection } from '@/components/home/cards'
-import { ClientSection } from '@/components/home/clients'
 import CtaSection from '@/components/home/cta'
 import FaqSection from '@/components/home/faqs'
 import { HeroSection } from '@/components/home/hero'
 import { IconCloudSection } from '@/components/home/icon-cloud'
 import { TabsSection } from '@/components/home/tabs'
 import TestimonialSection from '@/components/home/testimonial'
-import { VideoDialogSection } from '@/components/home/video'
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <TabsSection />
-      <ClientSection />
-      <VideoDialogSection/>
       <FeatureSection />
-      <AnimatedBeamSection/>
+      <AnimatedBeamSection />
       <TestimonialSection />
       <FaqSection />
-      <IconCloudSection/>
+      <IconCloudSection />
       <CtaSection />
     </>
   )
